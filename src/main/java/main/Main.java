36d6ccfb711f227e8963ba50d20888a17fdb3ae6
se4oev <pavluha1-5yxa@yandex.ru>
2021-10-26 22:19:54 +0300

    first version;

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,18 +1,20 @@
+package main;
+
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import servlets.AllRequestServlet;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        Frontend frontend = new Frontend();
-
-        Server server = new Server(8080);
+        AllRequestServlet allRequestServlet = new AllRequestServlet();
 
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        server.setHandler(context);
+        context.addServlet(new ServletHolder(allRequestServlet), "/*");
 
-        context.addServlet(new ServletHolder(frontend), "/authform");
+        Server server = new Server(8080);
+        server.setHandler(context);
 
         server.start();
         server.join();
