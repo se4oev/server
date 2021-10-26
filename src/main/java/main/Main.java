bc60b896e1cd61868d0e75564ff08270ddd68bce
se4oev <pavluha1-5yxa@yandex.ru>
2021-10-26 23:04:27 +0300

    solution of first exercise;

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -3,20 +3,22 @@ package main;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
-import servlets.AllRequestServlet;
+import servlets.MirrorRequestServlet;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        AllRequestServlet allRequestServlet = new AllRequestServlet();
-
+        MirrorRequestServlet mirrorRequestServlet = new MirrorRequestServlet();
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.addServlet(new ServletHolder(allRequestServlet), "/*");
+        context.addServlet(new ServletHolder(mirrorRequestServlet), "/mirror/*");
 
         Server server = new Server(8080);
         server.setHandler(context);
 
         server.start();
+
+        System.out.println("Server started");
+
         server.join();
     }
 }
